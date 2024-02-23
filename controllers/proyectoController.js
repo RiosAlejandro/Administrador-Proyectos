48dff90505584b7e60df9589133c31a072cbcc7b
RiosAlejandro <alexandro.aa86@gmail.com>
2024-02-23 18:03:11 -0300

    logica controllers de obtenerProyectos, nuevoProyecto y obtenerProyecto

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,8 +1,42 @@
-const obtenerProyectos = async (req, res) => {};
+import Proyecto from "../models/Proyecto";
 
-const nuevoProyecto = async (req, res) => {};
+const obtenerProyectos = async (req, res) => {
+  const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
 
-const obtenerProyecto = async (req, res) => {};
+  res.json(proyectos);
+};
+
+const nuevoProyecto = async (req, res) => {
+  //console.log(req.body);
+  //console.log(req.usuario);
+  const proyecto = new Proyecto(req.body);
+  proyecto.creador = req.usuario._id;
+
+  try {
+    const proyectoAlmacenado = await proyecto.save();
+    res.json(proyectoAlmacenado);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const obtenerProyecto = async (req, res) => {
+  const {id} = req.params;
+
+  const proyecto = await Proyecto.findById(id);
+
+  if(!proyecto) {
+    const error = new Error("No encontrado");
+    return res.status(404).json({msg: error.message});
+  }
+
+  if(proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(404).json({msg: error.message});
+  }
+
+  res.json(proyecto);
+};
 
 const editarProyecto = async (req, res) => {};
 
