fbaace7f301575f666a2a8e03ce2eb0a25baebe3
RiosAlejandro <alexandro.aa86@gmail.com>
2024-02-23 19:42:11 -0300

    ruta, router y controller de tareas

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import conectarBD from "./config/db.js";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
+import proyectoRoutes from "./routes/proyectoRoutes.js";
+import tareaRoutes from "./routes/tareaRoutes.js";
 
 const app = express();
 
@@ -14,7 +16,8 @@ conectarBD();
 
 //routing
 app.use("/api/usuarios", usuarioRoutes);
-app.use("/api/proyectos", usuarioRoutes);
+app.use("/api/proyectos", proyectoRoutes);
+app.use("/api/tareas", tareaRoutes);
 
 const port = process.env.PORT || 4000;
 
---
+++ routes/tareaRoutes.js
@@ -0,0 +1,20 @@
+import Express from "express";
+import {
+  agregarTarea,
+  obtenerTarea,
+  actualizarTarea,
+  eliminarTarea,
+  cambiarEstado, } from "../models/Tarea.js";
+import checkAuth from "../middlewares/checkAuth.js";
+
+const router = Express.Router();
+
+router.post("/", checkAuth, agregarTarea);
+
+router.get("/:id", checkAuth, obtenerTarea);
+router.put("/:id", checkAuth, actualizarTarea);
+router.delete("/:id", checkAuth, eliminarTarea);
+
+router.post("/estado/:id", checkAuth, cambiarEstado);
+
+export default router;
