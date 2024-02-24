5ee69096a07cd64bdb7c8b250fba3018ee28d26d
RiosAlejandro <alexandro.aa86@gmail.com>
2024-02-24 17:51:27 -0300

    logica controller de agregarTarea y obtenerTarea

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,6 +1,46 @@
-const agregarTarea = async (req, res) => {};
+import Proyecto from "../models/Proyecto.js";
+import Tarea from "../models/Tarea.js";
 
-const obtenerTarea = async (req, res) => {};
+const agregarTarea = async (req, res) => {
+  const {proyecto} = req.body;
+
+  const existeProyecto = await Proyecto.findById(proyecto);
+
+  if(!existeProyecto) {
+    const error = new Error("El proyecto no existe");
+    return res.status(404).json({msg: error.message});
+  }
+
+  if(existeProyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("No tienes los permisos para añadir tareas");
+    return res.status(403).json({msg: error.message});
+  }
+
+  try {
+    const tareaAlmacenada = await Tarea.create(req.body);
+    res.json(tareaAlmacenada);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const obtenerTarea = async (req, res) => {
+  const {id} = req.params;
+
+  const tarea = await Tarea.findById(id).populate("proyecto");
+
+  if(!tarea) {
+    const error = new Error("Tarea no encontrada");
+    return res.status(404).json({msg: error.message});
+  }
+
+  if(tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+    const error = new Error("Acción no válida");
+    return res.status(403).json({msg: error.message});
+  }
+
+  res.json(tarea);
+};
 
 const actualizarTarea = async (req, res) => {};
 
